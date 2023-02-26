e43fc8481a2df179216cd31e40711212540e96e6
MistaRinks <rinkanyakevin@gmail.com>
2023-02-26 08:10:44 -0800

    a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line."

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*This simple C program contains:
+ * #include - "copy contents of commands from"
+ * <stdio.h> - the standard C input/output header file
+ * main - main block
+ * puts - adds a newline character to the end of the specified argument and publishes it to the output stream
+ * return (0) - end program with a value of 0 if successful
+ * */
+
+
+#include <stdio.h>
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
