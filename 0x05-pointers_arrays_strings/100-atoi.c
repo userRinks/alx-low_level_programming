d0fbadd1ab3903afcfcaed44dfdfd699966fc24b
MistaRinks <rinkanyakevin@gmail.com>
2023-02-27 17:43:27 -0800

    [Advanced] Simple function that converts a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integer.
+ * @s: char type string
+ * return: integer converted
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int x = 0;
+	int y = -1;
+	int z = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			y = y * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			x = x * 10;
+			x -= (s[i] - '0');
+			z = 1;
+		}
+		else if (z == 1)
+			break;
+	}
+	x = y * x;
+	return (x);
+}
