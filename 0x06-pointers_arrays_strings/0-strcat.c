f624a79c99228b9de93c17ce194af58f3fae5720
MistaRinks <rinkanyakevin@gmail.com>
2023-03-02 08:15:41 -0800

    Concatenating two scripts

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,17 @@
 */
 
 char *_strcat(char *dest, char *src)
-
 {
 	int a = -1, i;
-	for (i = 0; dest[i] != '\0'; i++);
 
-	do
-	{
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+
+	do	{
 		a++;
 		dest[i] = src[a];
 		i++;
-	}
-	
+		}
 	while (src[a] != '\0');
 
 	return (dest);
