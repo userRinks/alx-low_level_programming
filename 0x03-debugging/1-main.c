0f257a297785a2022c60d454f41ce8221e617b01
MistaRinks <rinkanyakevin@gmail.com>
2023-03-05 19:25:30 -0800

    Debugged code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - no longer causes an infinite loop
+*	- bug in code edited out as comment
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+/*
+ * while (i < 10)
+ * {
+ *	putchar(i);
+ * }
+*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
