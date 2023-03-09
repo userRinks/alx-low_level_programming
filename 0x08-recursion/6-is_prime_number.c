c62f2b79d83be53022166ae0dcb13df3498a0962
MistaRinks <rinkanyakevin@gmail.com>
2023-03-09 07:17:12 -0800

    Instruction script

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,22 +9,23 @@
 
 int is_prime_number(int n)
 {
-	return (am_i_prime(n, 2));
+	return (check_prime(n, 2));
 }
 
 /**
- * am_i_prime - check if numbers less than are its factors
- * @n: integer
- * @p: integer
- * Return: integer value
+ * check_prime - check if all numbers < n can divide it
+ * @n: int
+ * @p: int
+ * Return: int
  */
 
-int am_i_prime(int n, int p)
+int check_prime(int n, int p)
 {
+
 	if (p >= n && n > 1)
 		return (1);
 	else if (n % p == 0 || n <= 1)
 		return (0);
 	else
-		return (am_i_prime(n, p + 1));
+		return (check_prime(n, p + 1));
 }
