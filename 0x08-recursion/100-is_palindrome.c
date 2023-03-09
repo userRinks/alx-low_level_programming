6ff519bfe509141544cc9decfeedb9d1f8e094b0
MistaRinks <rinkanyakevin@gmail.com>
2023-03-08 20:18:09 -0800

    [Advanced Task] Recurse to check for palindorme

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * last_index - return last index of string
+ * @s: pointer
+ * Return: integer
+ */
+
+int last_index(char *s)
+{
+	int i = 0;
+
+	if (*s > '\0')
+		i += last_index(s + 1) + 1;
+	return (n);
+}
+
+/**
+ * is_palindrome - check for palindrome
+ * @s: string
+ * @*s: pointer
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int fin = last_index(s);
+
+	return (am_i_pal(s, 0, fin - 1, fin % 2));
+}
+
+/**
+ * am_i_pal - check if number is palindrome
+ * @s: string
+ * @init: starts from initial letter to final letter
+ * @fin: starts from final letter to initial letter
+ * @p: variable integer
+ * Return: 0 or 1
+ */
+
+int am_i_pal(char *s, int init, int fin, int p)
+{
+	if ((init == fin && p != 0) || (init == fin + 1 && p == 0))
+		return (1);
+	else if (s[init] != s[fin])
+		return (0);
+	else
+		return (am_i_pal(s, init + 1, fin - 1, p));
+}
