71035d1e0d901ff220fff8bc9d4ec0a75e4bf73f
MistaRinks <rinkanyakevin@gmail.com>
2023-03-22 05:29:51 +0300

    [Header file] Data structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - data structure of dog
+ * @name: pointer to char
+ * @age: float type
+ * @owner: pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
