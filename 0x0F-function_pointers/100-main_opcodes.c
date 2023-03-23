402beba744f5e281052cf60cdb854f767a977a9f
MistaRinks <rinkanyakevin@gmail.com>
2023-03-23 06:01:21 +0300

    [Advanced task] Instruction script

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main -  prints the opcodes of its own main function.
@@ -9,20 +11,28 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, n;
+
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < atoi(argv[1]) - 1; i++)
-		printf("%02hhx ", ((char *)main)[i]);
-	printf("%02hhx\n", ((char *)main)[i]);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
