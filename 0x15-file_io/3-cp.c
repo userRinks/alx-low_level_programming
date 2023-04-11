7b811969f7fa802257f976e87b63a1d078353fd3
MistaRinks <rinkanyakevin@gmail.com>
2023-04-11 05:13:10 +0300

    [Instruction script] Copy content of file x into file y

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,120 +1,51 @@
 #include "main.h"
-#include <errno.h>
 
-#define BUF_SIZE 1024
-
-int open_file(char *file_path, int flags, mode_t mode);
-void copy_file(char *file_from, char *file_to, int *fd_from, int *fd_to);
-void close_file(int fd, char *file_path);
-void error(char *message, char *file_path);
-void error_exit(char *message);
+#define MAXSIZE 1204
+#define SE STDERR_FILENO
 
 /**
- * main - copy content of file into other file
- * @argc: argument count (no. of arguments)
- * @argv: argument vector (1st, 2nd or 3rd argument)
- * Return: (0) or code (97-100)
+ * main - create the copy bash script
+ * @ac: argument count
+ * @av: arguments as strings
+ * Return: 0
  */
-
-int main(int argc, char **argv)
+int main(int ac, char *av[])
 {
-	int fd_from, fd_to;
+	int input_fd, output_fd, istatus, ostatus;
+	char buf[MAXSIZE];
+	mode_t mode;
 
-	if (argc != 3)
-	{
-		error_exit("Usage: cp file_from file_to");
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2],
-			O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	copy_file(argv[1], argv[2], &fd_from, &fd_to);
-	return (0);
-}
-
-/**
- * open_file - open file
- * @file_path: file path
- * @flags: flags
- * @mode: permissions
- * Return: fd
- */
-int open_file(char *file_path, int flags, mode_t mode)
-{
-	int fd;
-
-	fd = open(file_path, flags, mode);
-	if (fd == -1)
-	{
-		error("Error: Can't open file ", file_path);
-	}
-	return (fd);
-}
-
-#define BUFFER_SIZE 1024
-/**
- * copy_file - function to copy file
- * @file_from: first argument
- * @file_to: second argument
- * @fd_from: from argument 1
- * @fd_to: to argument 2
- */
-
-void copy_file(char *file_from, char *file_to, int *fd_from, int *fd_to)
-{
-	char buffer[BUFFER_SIZE] = {0};
-	ssize_t bytes_read, bytes_written;
-	int fd_from_val = *fd_from;
-	int fd_to_val = *fd_to;
-
-	while ((bytes_read = read(fd_from_val, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to_val, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	if (ac != 3)
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+	input_fd = open(av[1], O_RDONLY);
+	if (input_fd == -1)
+		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (output_fd == -1)
+		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+	do {
+		istatus = read(input_fd, buf, MAXSIZE);
+		if (istatus == -1)
 		{
-			error("Error: Can't write to ", file_to);
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
 		}
-	}
-	close_file(fd_from_val, file_from);
-	close_file(fd_to_val, file_to);
-}
-
-/**
- * close_file - close file
- * @fd: int that opens files
- * @file_path: file path
- */
-void close_file(int fd, char *file_path)
-{
-	if (close(fd) == -1)
-	{
-		error("Error: Can't close fd ", file_path);
-	}
-}
-
-/**
- * error_exit - print error message
- * @message: message
- */
-void error_exit(char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(EXIT_FAILURE);
-}
-
-
-/**
- * error - print error message
- * @message: code
- * @file_path: file path
- */
-void error(char *message, char *file_path)
-{
-	dprintf(STDERR_FILENO, "%s%s\n", message, file_path);
-	if (file_path[0] != '\0')
-	{
-		dprintf(STDERR_FILENO, "%s\n", file_path);
-	}
-	perror("");
-	exit(EXIT_FAILURE);
+		if (istatus > 0)
+		{
+			ostatus = write(output_fd, buf, (ssize_t) istatus);
+			if (ostatus == -1)
+			{
+				dprintf(SE, "Error: Can't write to %s\n", av[2]);
+				exit(99);
+			}
+		}
+	} while (istatus > 0);
+	istatus = close(input_fd);
+	if (istatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
+	ostatus = close(output_fd);
+	if (ostatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
+	return (0);
 }
