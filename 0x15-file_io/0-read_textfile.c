e772dc6b9e7e10e564db36f3204a02936ed103bc
MistaRinks <rinkanyakevin@gmail.com>
2023-04-11 05:12:02 +0300

    [Instruction script] Read content and print to terminal

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,41 +12,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read = 0, bytes_written = 0;
+	ssize_t rcount, wcount;
 	char *buffer;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
-	fd = open(filename, O_RDONLY);		/* open */
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
-	{
 		return (0);
-	}
 
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_read = read(fd, buffer, letters);		/* read */
-	if (bytes_read == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);					/* write to POSIX terminal */
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
+	rcount = read(fd, buffer, letters);
+	if (rcount == -1)
 		return (0);
-	}
 
-	return (bytes_written);
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount == -1 || rcount != wcount)
+		return (0);
+	free(buffer);
+
+	close(fd);
+	return (wcount);
 }
