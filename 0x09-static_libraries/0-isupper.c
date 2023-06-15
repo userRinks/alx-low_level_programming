a8b0093290d343d2440c6ef6fa552718102e9f4b
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:23 +0300

    Check if uppercase

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c:	integer type number
+ * Return:	(1) if uppercase character
+ *		(0) if else
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
