c3e64736e88fc9b8d20a7d5d061159fe83ed17ee
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:24 +0300

    Convert string to int

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string value to int value
+ * @s: pointer to string
+ * Return: converted value (atoi)
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
