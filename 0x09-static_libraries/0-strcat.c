1c8cc20169e5fcbefdadf8654f05b9996e6b721d
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:23 +0300

    Concatenate strings

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenate strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: strcat
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
