08948219976b6da5e5df162ab560349ca23025b8
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:24 +0300

    Compare strings

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare and display difference in string values
+ * @s1: string 1
+ * @s2: string 2
+ * Return: strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
