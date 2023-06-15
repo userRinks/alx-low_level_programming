83657325f673e3247d22a0d86cbabf256497b6b3
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:23 +0300

    Find length of prefix substring

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - print index of first character found
+ * @s: pointer to string
+ * @accept: pointer
+ * Return: strspn
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
