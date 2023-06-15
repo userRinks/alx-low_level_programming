ffea05ac23ec3bd6bc7d3c9b671caa3e09c1efe2
MistaRinks <rinkanyakevin@gmail.com>
2023-06-15 06:32:23 +0300

    Copy and concatenate string

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string values and concatenates them
+ * @dest: pointer
+ * @src: pointer
+ * @n: int
+ * Return: strncpy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
