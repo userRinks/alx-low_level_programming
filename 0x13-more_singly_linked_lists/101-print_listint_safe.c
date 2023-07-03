61f14ba663e06887db410251e714df3230e9fd45
MistaRinks <rinkanyakevin@gmail.com>
2023-07-04 00:33:25 +0300

    [Advanced] Print list elements safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_listint_safe - prints a linked list "safely"
@@ -10,22 +8,35 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
+	const listint_t *slow, *fast;
 	size_t count = 0;
 
-	current = head;
-	while (current != NULL)
+	slow = head;
+	fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %d\n", (void *)slow, slow->n);
 		count++;
 
-		if (current < current->next)
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
 			break;
 		}
+	}
 
-		current = current->next;
+	if (slow == NULL || fast == NULL || fast->next == NULL)
+	{
+		while (head != NULL)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			count++;
+			head = head->next;
+		}
 	}
 
 	return (count);
