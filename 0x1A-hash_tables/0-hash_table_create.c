5b7a770cb4fae8f3c6e95cdafbb5a5c04e69cebf
userRinks <rinkanyakevin@gmail.com>
2023-12-01 23:51:52 +0300

    Create a hash table without freeing memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,31 +6,31 @@
  * @size: The size of the array.
  *
  * Return: A pointer to the newly created hash table,
- *         or NULL if something went wrong.
+ *			or NULL if something went wrong.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of hash_node_t pointers */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+	/* Allocate memory for the array of hash_node_t pointers */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    new_table->size = size;
+	new_table->size = size;
 
-    /* Initialize each element of the array to NULL */
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+	/* Initialize each element of the array to NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
-    return (new_table);
+	return (new_table);
 }
